995440fae24bdfc175db0499240b7a7c053342ec
gyhyz917 <zhuyihan0826@outlook.com>
2025-08-20 16:18:09 +0800

    feat:finish parsing path.

--- src/parser/ParsePath.cpp
+++ src/parser/ParsePath.cpp
@@ -1,9 +1,53 @@
 #include "parser.h"
 #include <memory>
+#include <stdexcept>
+#include <vector>
 
 namespace JaneZ {
 std::unique_ptr<Path> Parser::parse_path() {
-
+    std::vector<PathIdentSegment> current;
+    if(currentPos >= tokens.size()){
+        throw std::runtime_error("End of Program.");
+    }
+    PathIdentSegment tmp;
+    if(tokens[currentPos].type == kIDENTIFIER){
+        tmp.type = IDENTIFIER;
+    }else if(tokens[currentPos].type == kSELF_TYPE){
+        tmp.type = SELF_TYPE;
+    }else if(tokens[currentPos].type == kSELF){
+        tmp.type = SELF;
+    }else{
+        throw std::runtime_error("Wrong type when parsing path.");
+    }
+    tmp.identifier = tokens[currentPos].value;
+    currentPos ++;
+    if(currentPos >= tokens.size()){
+        throw std::runtime_error("End of Program.");
+    }
+    current.push_back(tmp);
+    while(tokens[currentPos].type == kPATHSEP){
+        currentPos ++;
+        if(currentPos >= tokens.size()) {
+            throw std::runtime_error("End of Program.");
+        }
+        PathIdentSegment tp;
+        if(tokens[currentPos].type == kIDENTIFIER){
+            tp.type = IDENTIFIER;
+        }else if(tokens[currentPos].type == kSELF_TYPE){
+            tp.type = SELF_TYPE;
+        }else if(tokens[currentPos].type == kSELF){
+            tp.type = SELF;
+        }else{
+            throw std::runtime_error("Wrong type when parsing path.");
+        }
+        tp.identifier = tokens[currentPos].value;
+        currentPos ++;
+        if(currentPos >= tokens.size()){
+            throw std::runtime_error("End of Program.");
+        }
+        current.push_back(tp);
+    }
+    return std::make_unique<Path>(std::move(current));
 }
 
 }
