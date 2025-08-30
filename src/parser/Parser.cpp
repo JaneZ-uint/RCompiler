64a6ac077711b040a061dd0a40ba891348faf29b
gyhyz917 <zhuyihan0826@outlook.com>
2025-08-30 20:47:21 +0800

    feat: add interface of parser.

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -3,7 +3,13 @@
 
 namespace JaneZ {
 std::unique_ptr<ASTRootNode> Parser::parse() {
-    
+    std::vector<std::unique_ptr<Item>> child;
+    for(int i = 0;i < tokens.size();i ++) {
+        std::unique_ptr<Item> current;
+        current = parse_item();
+        child.push_back(std::move(current));
+    }
+    return std::make_unique<ASTRootNode>(std::move(child));
 }
 
 }
