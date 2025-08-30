54e70aaf127ae0d19dc6afa464be1c2b6d141c3a
gyhyz917 <zhuyihan0826@outlook.com>
2025-08-30 23:22:56 +0800

    feat: add parser in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "src/lexer/lexer.h"
+#include "src/parser/parser.h"
 # include "src/simplifier/simplifier.h"
+#include "src/token/token.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
     JaneZ::Simplifier simplifier("8.rs");
     std::string source_code = simplifier.work();
     //simplifier.print();
     JaneZ::Lexer lexer(source_code);
-    lexer.work();
-    lexer.print();
+    std::vector<JaneZ::Token> tokens = lexer.work();
+    //lexer.print();
+    JaneZ::Parser parser(tokens);
+    parser.parse();
 }
