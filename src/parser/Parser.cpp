7866007e966ffd7e4b0a5b3c51a2d94ce6988e32
gyhyz917 <zhuyihan0826@outlook.com>
2025-09-04 18:19:46 +0800

    fix: bug in parser interface.

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -4,10 +4,13 @@
 namespace JaneZ {
 std::unique_ptr<ASTRootNode> Parser::parse() {
     std::vector<std::unique_ptr<Item>> child;
-    for(int i = 0;i < tokens.size();i ++) {
+    while(true) {
         std::unique_ptr<Item> current;
         current = parse_item();
         child.push_back(std::move(current));
+        if(currentPos >= tokens.size()) {
+            break;
+        }
     }
     return std::make_unique<ASTRootNode>(std::move(child));
 }
