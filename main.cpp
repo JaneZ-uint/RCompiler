bbb814018af20ebdcfd007cf08a89707bbfa4122
gyhyz917 <zhuyihan0826@outlook.com>
2025-09-26 17:37:46 +0800

    feat: add part of NameResolver.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,26 @@
 # include "src/simplifier/simplifier.h"
 # include "src/token/token.h"
 # include "src/ast/PrintVisitor.h"
-# include <iostream>
 #include <memory>
+#include <stdexcept>
 # include <string>
 # include <vector>
 
 int main(){
-    JaneZ::Simplifier simplifier("../RCompiler-Testcases/semantic-1/type8/type8.rx");
-    //JaneZ::Simplifier simplifier("1.rs");
-    std::string source_code = simplifier.work();
-    //simplifier.print();
-    JaneZ::Lexer lexer(source_code);
-    std::vector<JaneZ::Token> tokens = lexer.work();
-    //lexer.print();
-    JaneZ::Parser parser(tokens);
-    std::shared_ptr<JaneZ::ASTRootNode> root = parser.parse();
-    JaneZ::PrintVisitor printer;
-    printer.visit(*root);
+    try{
+        JaneZ::Simplifier simplifier("../RCompiler-Testcases/semantic-1/type8/type8.rx");
+        //JaneZ::Simplifier simplifier("1.rs");
+        std::string source_code = simplifier.work();
+        //simplifier.print();
+        JaneZ::Lexer lexer(source_code);
+        std::vector<JaneZ::Token> tokens = lexer.work();
+        //lexer.print();
+        JaneZ::Parser parser(tokens);
+        std::shared_ptr<JaneZ::ASTRootNode> root = parser.parse();
+        JaneZ::PrintVisitor printer;
+        printer.visit(*root);
+    }catch(std::runtime_error){
+        return 0;
+    }
+    return 1;
 }
---
+++ src/semantic/checker.h
@@ -0,0 +1,10 @@
+# pragma once
+
+namespace JaneZ {
+class Checker{
+public:
+    void semantic_check(){
+        
+    }
+};
+}
