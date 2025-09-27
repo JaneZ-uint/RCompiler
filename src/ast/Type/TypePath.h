f39435a6a5f163e3bab876efdfbf17acca2f8d76
gyhyz917 <zhuyihan0826@outlook.com>
2025-09-28 01:09:23 +0800

    feat: finish nameresolver item part.

--- src/ast/Type/TypePath.h
+++ src/ast/Type/TypePath.h
@@ -2,11 +2,15 @@
 
 #include "../ast.h"
 #include <memory>
+#include "../../semantic/symbol.h"
+
 namespace JaneZ {
 class Path;
 class TypePath : public ASTNode {
 public:
     std::shared_ptr<Path> typePath;
+
+    std::shared_ptr<Symbol> resolvedSymbol = nullptr;
 
     TypePath(std::shared_ptr<Path> tp) : typePath(std::move(tp)) {}
 
