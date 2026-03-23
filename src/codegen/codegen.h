4f78da822e3b57beca9f0967459d70d6aa2d2e5d
gyhyz917 <zhuyihan0826@outlook.com>
2026-03-24 00:38:05 +0800

    feat: finish upper interface.

--- src/codegen/codegen.h
+++ src/codegen/codegen.h
@@ -1,10 +1,17 @@
 # pragma once
 # include "../ir/IRRoot.h"
+# include "InstrSelect.h"
+# include "ASMPrinter.h"
 #include <memory>
 
 namespace JaneZ {
 class codegen{
 public:
-    void generate(std::shared_ptr<IRRoot> root);
+    void generate(std::shared_ptr<IRRoot> root){
+        InstrSelect selector;
+        selector.select(root);
+        ASMPrinter printer(selector.asmBlocks);
+        printer.print();
+    }
 };
 }
